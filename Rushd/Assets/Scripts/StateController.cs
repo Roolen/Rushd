303fc733ea8fe7f84f67be0a5d6c895ead6b92a7
Artem <zhimovskij@yandex.ru>
2018-05-20 20:16:35 +0500

    Dev: добавлен StateController и через него реализован выход из приложения

---
+++ Rushd/Assets/Scripts/StateController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class StateController : MonoBehaviour {
+
+    public enum States
+    {
+        Play = 0,
+        Stop = 1,
+        Exit = 2
+    }
+
+	void Start ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Изменяет состояние игры.
+    /// </summary>
+    /// <param name="stateNew">Новое состояние</param>
+    public void ChangeState(States stateNew)
+    {
+        if (stateNew == States.Play) Play();
+
+        else if (stateNew == States.Stop) Stop();
+
+        else if (stateNew == States.Exit) Exit();
+    }
+
+    public void ChangeState(int i)
+    {
+        States newState = (States)i; 
+
+        ChangeState(newState);
+    }
+
+    private bool Play()
+    {
+        try
+        {
+            Time.timeScale = 1.0f;
+            Debug.Log("Time play");
+
+            return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.Message);
+            return false;
+        }
+    }
+
+    private bool Stop()
+    {
+        try
+        {
+            Time.timeScale = 0.0f;
+            Debug.Log("Time stop");
+
+            return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.Message);
+            return false;
+        }
+    }
+
+    private bool Exit()
+    {
+        try
+        {
+            Application.Quit();
+            Debug.Log("Application quit");
+
+            return true;
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+            return false;
+        }
+    }
+}
