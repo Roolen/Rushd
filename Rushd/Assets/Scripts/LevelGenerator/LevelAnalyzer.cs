e6ba25cdb4c4bc9cf1d5a65142ffe0973fbb9690
Artem <zhimovskij@yandex.ru>
2018-05-23 23:43:31 +0500

    Dev: добавлена загрузка файла уровня в "LevelAnalyzer" (#7)

---
+++ Rushd/Assets/Scripts/LevelGenerator/LevelAnalyzer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.LevelGenerator
+{
+    public class LevelAnalyzer : MonoBehaviour
+    {
+        public LevelData dates;
+
+        private void Start ()
+        {
+            FileInfo fileLevel = null;
+            fileLevel = new FileInfo("D:\\WorkingDocument\\Rushd\\Level_1.lvl");
+
+            if (fileLevel.Exists)
+            {
+                AnalysisOfFileLevel(fileLevel);
+            }
+            else
+            {
+                Debug.LogError("EL_0001: Ошибка загрузки файла уровня");
+                SceneManager.LoadScene(0);
+            }
+        }
+
+        private void AnalysisOfFileLevel(FileInfo fileLevel)
+        {
+            
+        }
+
+        private void AnalysisOfDataLevel(FileInfo fileLevel)
+        {
+
+        }
+    }
+}
