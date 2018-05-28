c0b8dccf1bf04452102ef5dbacb737a89b6b05e9
Artem <zhimovskij@yandex.ru>
2018-05-28 22:16:09 +0500

    Dev: разарботан строитель play menu (#11)

---
+++ Rushd/Assets/Scripts/BuilderForPlayMenu.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Xml;
+using Assets.Scripts.LevelGenerator;
+using UnityEditor.IMGUI.Controls;
+
+namespace Assets.Scripts
+{
+    public class BuilderForPlayMenu : MonoBehaviour
+    {
+        public GameObject contentArea;
+        public Button buttonLevel;
+
+        void Start ()
+        {
+		    BuildPlayMenu();
+        }
+
+        private bool BuildPlayMenu()
+        {
+            //DirectoryInfo dir = new DirectoryInfo("Map\\");
+            DirectoryInfo dir = new DirectoryInfo("D:\\Map");
+
+            if (!dir.Exists)
+            {
+                Debug.LogError("EL_001: Проблема с загрузкой файла игрового уровня");
+                return false;
+            }
+
+            foreach (FileInfo levelFile in dir.GetFiles())
+            {
+                if (!levelFile.Exists)
+                {
+                    Debug.LogError("EL_001: Проблема с загрузкой файла игрового уровня");
+                    continue;
+                }
+
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load(levelFile.FullName);
+
+                XmlElement xmlRoot = xmlDoc.DocumentElement;
+
+                Button instanceButton = Instantiate(buttonLevel);
+                instanceButton.transform.parent = contentArea.transform;
+
+                LevelInfo level = instanceButton.GetComponent<LevelInfo>();
+
+                if (xmlRoot.Attributes.Count > 0)
+                {
+
+                    XmlNode attributeName = xmlRoot.Attributes.GetNamedItem("Name");
+
+                    if (attributeName != null)
+                    {
+                        level.NameLevel = attributeName.Value;
+                    }
+                    else
+                    {
+                        Debug.LogError("EL_002: Некорректные атрибуты уровня");
+                    }
+
+                    XmlNode attributeDescriprion = xmlRoot.Attributes.GetNamedItem("Description");
+
+                    if (attributeDescriprion != null)
+                    {
+                        level.DescriptionLevel = attributeDescriprion.Value;
+                    }
+                    else
+                    {
+                        Debug.LogError("EL_002: Некорректные атрибуты уровня");
+                    }
+
+                    XmlNode attributeDifficult = xmlRoot.Attributes.GetNamedItem("Difficult");
+
+                    if (attributeDifficult != null)
+                    {
+                        int indexDifficult = Convert.ToInt32(attributeDifficult.Value);
+                        level.DifficultLevel = (Difficult)indexDifficult;
+                    }
+                    else
+                    {
+                        Debug.LogError("EL_002: Некорректные атрибуты уровня");
+                    }
+
+                    XmlNode attributeHeight = xmlRoot.Attributes.GetNamedItem("Height");
+
+                    if (attributeHeight != null)
+                    {
+                        level.HeightLevel = Convert.ToInt32(attributeHeight.Value);
+                    }
+                    else
+                    {
+                        Debug.LogError("EL_002: Некорректные атрибуты уровня");
+                    }
+
+                    XmlNode attributeWeight = xmlRoot.Attributes.GetNamedItem("Weight");
+
+                    if (attributeWeight != null)
+                    {
+                        level.WeightLevel = Convert.ToInt32(attributeWeight.Value);
+                    }
+                    else
+                    {
+                        Debug.LogError("EL_002: Некорректные атрибуты уровня");
+                    }
+                }
+                else
+                {
+                    Debug.LogError("EL_002: Некорректные атрибуты уровня");
+                }
+
+
+                {
+                    level.textButtonLevel.text = level.nameLevel;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Rushd/Assets/Scripts/LevelInfo.cs
@@ -0,0 +1,82 @@
+﻿using Assets.Scripts.LevelGenerator;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class LevelInfo : MonoBehaviour
+    {
+        public Text textButtonLevel;
+
+        public string nameLevel;
+        private string descriptionLevel;
+        private Difficult difficultLevel;
+        private int heightLevel;
+        private int weightLevel;
+
+        public string NameLevel
+        {
+            get
+            {
+                return nameLevel;
+            }
+
+            set
+            {
+                nameLevel = value;
+            }
+        }
+
+        public string DescriptionLevel
+        {
+            get
+            {
+                return descriptionLevel;
+            }
+
+            set
+            {
+                descriptionLevel = value;
+            }
+        }
+
+        public Difficult DifficultLevel
+        {
+            get
+            {
+                return difficultLevel;
+            }
+
+            set
+            {
+                difficultLevel = value;
+            }
+        }
+
+        public int HeightLevel
+        {
+            get
+            {
+                return heightLevel;
+            }
+
+            set
+            {
+                heightLevel = value;
+            }
+        }
+
+        public int WeightLevel
+        {
+            get
+            {
+                return weightLevel;
+            }
+
+            set
+            {
+                weightLevel = value;
+            }
+        }
+    }
+}
