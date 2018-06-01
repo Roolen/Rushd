d0688950d54d6efbb79199a6a0a18b200c42a7e4
Dobroslav <school5dobroslav@gmail.com>
2018-06-01 17:44:53 +0500

    Test: Добавлено тестовое парение

---
+++ Rushd/Assets/Scripts/TesterScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesterScript : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+    // C# example.
+    void Update()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+       
+
+    // This would cast rays only against colliders in layer 8.
+    // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+    layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 2, 0);
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //Debug.Log("Did Hit");
+        }
+        else
+        {
+
+            GetComponent<Rigidbody>().AddForce(0, -2, 0);
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            //Debug.Log("Did not Hit");
+        }
+    }
+}
+
+
